0baeaf5729e50520e9c05ed01b103a82f7f57698
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-12 10:36:55 -0400

    DELIV 2 DONE OMGgit add .! IM NOT STUPID

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from './QuestionItem';
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      let req = await fetch(`http://localhost:4000/questions`)
+      let res = await req.json()
+      console.log("data rtn", res)
+      setQuestions(res)
+    })()
+  }, [])
+  
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+      {
+        questions.map((question) => {
+          return (
+            <QuestionItem key={question.id} question={question}/>
+          )
+        })
+      }
+      </ul>
     </section>
   );
 }
